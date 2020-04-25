786e5354dfd7af336da31ff905749344b961e775
DXsmiley <dxsmiley@hotmail.com>
2020-04-25 20:28:31 +1000

    Better way of running the sever

---
+++ notes.md
@@ -0,0 +1,8 @@
+# Notes to myself
+
+Update the server with:
+
+```
+git pull
+pm2 restart rtex
+```
--- runserver.sh
+++
@@ -1,6 +0,0 @@
-# Stop the server that's running and start it again
-# This is for personal use you should read this code before running it
-
-pkill python3
-mkdir -p temp
-nohup python3 src/server.py 80 --release &
